8d9f1b71d6ea726c58628cd2fae51523e81b4d89
Adrián Tóth <adricko28@gmail.com>
2017-11-28 16:28:49 +0100

    INIT: src/

---
+++ src/ims.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+#include "modul.hpp"
+
+int main(int argc, char* argv[]) {
+
+	(void)argc;
+	(void)argv;
+
+	std::cout << "=IMS=" << std::endl;
+
+	return 0;
+}
---
+++ src/modul.hpp
@@ -0,0 +1,4 @@
+#ifndef __modul_hpp
+#define __modul_hpp
+
+#endif
